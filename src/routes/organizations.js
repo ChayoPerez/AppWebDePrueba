07232252609080812b4928a447bdf96dc30a6f50
Chayoperez <raperez14@uc.cl>
2020-12-07 15:02:42 -0300

    base working

--- src/routes/organizations.js
+++ src/routes/organizations.js
@@ -10,7 +10,7 @@ const PERMITTED_FIELDS = [
 
 router.param("id", async (id, ctx, next) => {
     // usamos ctx.state para enviar algo a otro middleware
-    // ctx.orm.user significa el modelo user dentro de sequelize
+    // ctx.orm.user significa el modelo dentro de sequelize
     const organization = await ctx.orm.organization.findByPk(id);
     if (!organization) ctx.throw(404); // si no existe retorna not found
     ctx.state.organization = organization;
@@ -21,47 +21,43 @@ router.get("organizations", "/", async (ctx) => {
 
     //const { cloudinary } = ctx.state;
     //const organization = ctx.state.currentOrganization;
-    const organizations = await ctx.orm.organization.findAll({ include: "organization" });
+    const organizations = await ctx.orm.organization.findAll();
     /* organizations.forEach(org => {
       org.dataValues.cloudinaryImage = cloudinary.url(org.image);
     }); */
 
     await ctx.render("organizations/index", {
       organizations,
+      //getOrganizationPath: ctx.router.url("organization", { id: recipe.user.id }),
     });
   });
   
-  router.get("organizations-new", "/organizations/new", async (ctx) => {
+  router.get("organizations-new", "/new", async (ctx) => {
     const organization = ctx.orm.organization.build();
     return ctx.render("organizations/new", {
       organization,
+      createOrgPath: ctx.router.url("organizations-create"),
     });
   });
   
  
   router.post("organizations-create", "/", async (ctx) => {
     const organization = ctx.orm.organization.build(ctx.request.body);
-    /* const { cloudinary } = ctx.state;
-    const { image } = ctx.request.files;
-    let recipeImageId;
-    if (image.size > 0) {
-      const uploadedImage = await cloudinary.uploader.upload(image.path);
-      recipeImageId = uploadedImage.public_id;
-    }
-    recipe.image = recipeImageId; */
     try {
       await organization.save({ fields: PERMITTED_FIELDS });
-      ctx.redirect(ctx.router.url("organization", { id: organization.id }));
+      ctx.redirect(ctx.router.url("organizations"));
     } catch (error) {
+      //  Si hay un error vuelve a new
       await ctx.render("organizations/new", {
+        createOrgPath: ctx.router.url("organizations-create"),
         errors: error.errors,
       });
     }
   });
 
-  router.get("organization-show", "organization/:id", async (ctx) => {
-    // await ctx.orm.like.destroy({where: {}}).then(function () {});
-    const organization = await ctx.orm.organization.findByPk(id);
+  router.get("organization-show", "/:id", async (ctx) => {
+    const orgId = ctx.params.id
+    const organization = await ctx.orm.organization.findByPk(orgId);
     return ctx.render("organizations/show", {
       organization,
     });
